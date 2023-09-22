e7bc0bd85d1276e236af32758cd2bb9f388e2e02
EunKyo Chae <dmsry1022@gmail.com>
2023-09-22 20:29:00 +0900

    add: 점수비교해서 높은 사람 메서드

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,19 +3,6 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-
-    static final int EXCEPT_VALUE = -1; // 예외사항 -> 책이 연달아 펴지질 않음
-    static final int POBI_WIN = 1; // 포비가 이겼을 때 1
-    static final int CRONG_WIN = 2; // 크롱이 이겼을 때 2
-    static final int SAME_VALUE = 0; // 둘이 무승부였을 때 0
-    static final int BOOK_MIN_VALUE = 1; // 책의 최소페이지 1
-    static final int BOOK_MAX_VALUE = 400; // 책의 최대페이지 400
-
-    public static int solution(List<Integer> pobi, List<Integer> crong) { // 해결하는 메서드인듯
-        int answer = Integer.MAX_VALUE; // answer 과 최대값
-        return answer;
-    }
-
     /*
     * 어떠한 메서드를 만들것인가?
     * 주어진것: 최소 1페이지~최대 400페이지의 책, 포비가 이기면 = 1, 크롱이 이기면 2 무승부 0 , 예외 -1
@@ -25,29 +12,61 @@ class Problem1 {
     * 3. 각 페이지를 곱하는 메서드
     * 4. 더한 값과 곱한 값의 최대값을 뽑는 메서드
     * */
-    public static void checkRightAndLeft(List<Integer> pages){ // 왼쪽 오른쪽
-        int left = pages.get(0);
-        int right = pages.get(1);
+    public void leftRightPage(List<Integer> pages){
+        int left = pages.get(0); // left는 홀수
+        int right = pages.get(1); // right = 짝수
+
+        if(1 < left || left < 400){
+
+        }
     }
 
-    public static int[] changeToNum(int pageNum){ // 페이지 100의자리 10의 자리 등등
+    public int[] changeToNum(int pageNum){ // 페이지 100의자리 10의 자리 등등
         int onePage = pageNum % 10;// get(0), get(1) 왼, 오 일의 자리
         int twoPage = (pageNum % 100 ) / 10;
         int threePage = ((pageNum % 1000) / 100);
 
-        int[] digit = { onePage, twoPage, threePage};
+        int[] digits = { onePage, twoPage, threePage};
 
-        return digit;
+        return digits;
     }
 
-    public static int numAddPage(int pageNum){ // 더하는 메서드
-        int[] digit = changeToNum(pageNum);
+    public int numAddMultiplerPage(int pageNum){ // 더하는 곱하는 메서드
+        int[] digits = changeToNum(pageNum);
         int addNum = 0;
-        for(int digits : digit){
-            addNum += digits;
+        int multiplerNum = 1;
+        for(int digit : digits){
+            addNum += digit;
+            multiplerNum *= digit;
         }
+        int MAX_VALUE = Math.max(addNum, multiplerNum);
+        return MAX_VALUE;
+    }
 
-        return addNum;
+    public int scoreCompareToNum(List<Integer> pages){ // 점수를 비교해 가장 높은 사람
+        int leftScore = numAddMultiplerPage(pages.get(0));
+        int rightSocre = numAddMultiplerPage(pages.get(1));
+
+        return Math.max(leftScore, rightSocre);
+    }
+    public static int solution(List<Integer> pobi, List<Integer> crong) { // 해결하는 메서드인듯
+        Problem1 b1 = new Problem1();
+
+        b1.leftRightPage(pobi);
+        b1.leftRightPage(crong);
+
+        int pobiScore = b1.scoreCompareToNum(pobi);
+        int crongScore = b1.scoreCompareToNum(crong);
+
+        if(pobiScore > crongScore)
+            return 1;
+        if (pobiScore < crongScore)
+            return 2;
+        return 0;
+
+
+        //int answer = Integer.MAX_VALUE; // answer 과 최대값
+        //return answer;
     }
 
 }
